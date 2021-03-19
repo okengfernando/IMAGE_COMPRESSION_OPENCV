c5e88e6f5b47f805adb96d5426734bfa0eba259c
fernando <okengfernando@github.com>
2021-03-19 18:45:51 +0300

    refactored test cases

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ class main_functions_testing(unittest.TestCase):
 
     def test_case(self):
         pass
-    
+
     def test_out_from_the_Representational_function(self):
         self.assertEqual(main.Representational(1.2, 2.5, 4.3) ,  1.5664999999999998)
     
@@ -14,4 +14,8 @@ class main_functions_testing(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main()                   
+
+
+
+    
