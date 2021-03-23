57ed9025f84a705894829b49eb5d859a39a010f7
fernando <okengfernando@github.com>
2021-03-23 19:36:14 +0300

    added more test cases

--- test.py
+++ test.py
@@ -7,6 +7,12 @@ class main_functions_testing(unittest.TestCase):
     def test_case(self):
         pass
 
+    def test_image_compression(self):
+        pass
+
+    def test_image_oixel_toarray_conversion(self):
+        pass
+
     def test_out_put_from_the_Representational_function(self):
         self.assertEqual(main.Representational(1.2, 2.5, 4.3) ,  1.5664999999999998)
     
