f61bf03a9130544774f7c92eaef2bbdc9caeaf21
fernando <okengfernando@github.com>
2021-03-30 19:32:30 +0300

    added more unit tests for the Represenatational Function

--- test.py
+++ test.py
@@ -16,6 +16,9 @@ class main_functions_testing(unittest.TestCase):
     def test_out_put_from_the_Representational_function(self):
         self.assertEqual(main.Representational(1.2, 2.5, 4.3) ,  1.5664999999999998)
     
+    def test_out_type_from_the_Represenatational(self):
+        self.assertTrue(type(main.Representational(1.2, 2.5, 4.3)) == float)
+    
 
 
 
